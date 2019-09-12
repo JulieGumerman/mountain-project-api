6892533bc2a0aab6e888d489fb03832149059e2f
bodhibrdr <julie.gumerman@gmail.com>
2019-09-12 16:44:34 -0600

    getting there

--- src/components/RouteInfo.js
+++ src/components/RouteInfo.js
@@ -1,5 +1,4 @@
 import React from "react";
-import mapAgeCleaner from "map-age-cleaner";
 
 
 const RouteInfo = props => {
@@ -7,9 +6,15 @@ const RouteInfo = props => {
         <div key={props.route.id}>
             <img src={props.route.imgSqSmall} alt={props.route.name}/>
             <h3>{props.route.name}</h3>
-            <p><strong>Location: </strong>{props.route.location.map(place => place)}</p>
+            <p><strong>Location: </strong>
+                <ul>
+                    {props.route.location.map(place => 
+                        <li>{place}</li>)}
+                </ul>
+            </p>
             <p><strong>Type: </strong>{props.route.type}</p>
             <p><strong>Pitches: </strong>{props.route.rating}</p>
+            <a href={props.route.url}><i class="fas fa-mountain"></i></a>
         </div>
     );
 }
